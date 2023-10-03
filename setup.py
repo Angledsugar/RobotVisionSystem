ad8871e26648631970d34126607e9889f7f71f95
ccy <angledsugar@gmail.com>
2023-10-03 17:59:53 +0900

    Initial

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'RobotVisionSystem'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ccy',
+    maintainer_email='angledsugar@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
